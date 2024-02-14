45a220ee90bb9ac1cbe0a1e83ddf0180cef3fa59
hhshiblu <hhshiblu5555@gmail.com>
2024-02-14 22:35:21 +0600

    j

--- extra.js
+++
@@ -1,7 +0,0 @@
-DATABASE_URL="mongodb+srv://hhshiblu55:hhshiblu55@cluster0.q46cmnb.mongodb.net/?retryWrites=true&w=majority"
-NEXTAUTH_SECRET="./jfhadfasffjhasfh@@$#jhnfdjgnsdg5788eythfndasbfasjfashf7sadfja%$^"
-NEXTAUTH_URL="http://localhost:3000/"
-NEXT_AWS_S3_REGION=ap-south-1
-NEXT_AWS_S3_ACCESS_KEY_ID=AKIAVQHLXFOIBZELEX4A
-NEXT_AWS_S3_SECRET_ACCESS_KEY=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
-NEXT_AWS_S3_BUCKET_NAME=rajdhola
